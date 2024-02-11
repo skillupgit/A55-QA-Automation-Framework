3700e88e1a87ab7a71bf436cce168793a7a9fae1
Muhammad Fraz <skillupcanada@outlook.com>
2024-02-11 16:36:44 -0500

    Update Login.test

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,7 +11,7 @@ import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
-    public void loginEmptyEmailPassword() {
+    public void navigateToKoel() {
 
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
@@ -19,7 +20,7 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://qa.koel.app/";
+        String url = "https://demo.koel.dev/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
@@ -30,29 +31,27 @@ public class LoginTests extends BaseTest {
         //Pre-Condition
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
         //Declaration
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
         //Steps 1: Open Browser and navigate to Koel app.
-        String url = "https://qa.koel.app/";
+        String url = "https://demo.koel.dev/";
         driver.get(url);
-
         //Step 2: Enter email
         WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
         emailField.clear();
-        emailField.sendKeys("demo@class.com");
-
+        emailField.sendKeys("demo@koel.dev");
         //Step 3: Enter Password
         WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
         passwordField.clear();
-        passwordField.sendKeys("te$t$tudent");
+        passwordField.sendKeys("demo");
         //Step 4: Click on Login button
         WebElement loginBtn =  driver.findElement(By.cssSelector("button[type='submit']"));
         loginBtn.click();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         //Assertion (expected vs actual)
-        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+        WebElement avatarIcon = driver.findElement(By.cssSelector(".view-profile"));
         Assert.assertTrue(avatarIcon.isDisplayed());
         //Quit the browser
         driver.quit();
@@ -63,19 +62,17 @@ public class LoginTests extends BaseTest {
         //Pre-Condition
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
         //Declaration
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
         //Steps 1: Open Browser and navigate to Koel app.
-        String url = "https://qa.koel.app/";
+        String url = "https://demo.koel.dev/";
         driver.get(url);
-
         //Step 2: Enter email
         WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
         emailField.clear();
         emailField.sendKeys("invalid@class.com");
-
         //Step 3: Enter Password
         WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
         passwordField.clear();
@@ -95,12 +92,13 @@ public class LoginTests extends BaseTest {
         //Pre-Condition
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
         //Declaration
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         //Steps 1: Open Browser and navigate to Koel app.
-        String url = "https://qa.koel.app/";
+        String url = "https://demo.koel.dev/";
         driver.get(url);
 
         //Step 2: Enter email
